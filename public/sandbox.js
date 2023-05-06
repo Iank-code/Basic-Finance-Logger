128ec6207132abdcade34f7eadba4ccaa295e5c7
ianK-code <kian99564@gmail.com>
2023-05-06 11:21:27 +0300

    Learnt about turples

--- public/sandbox.js
+++ public/sandbox.js
@@ -11,12 +11,14 @@ const details = document.querySelector("#details");
 const amount = document.querySelector("#amount");
 form.addEventListener("submit", (e) => {
     e.preventDefault();
+    let values;
+    values = [tofrom.value, details.value, amount.valueAsNumber];
     let doc;
     if (type.value === "invoice") {
-        doc = new Invoice(tofrom.value, details.value, amount.valueAsNumber);
+        doc = new Invoice(...values);
     }
     else {
-        doc = new Payment(tofrom.value, details.value, amount.valueAsNumber);
+        doc = new Payment(...values);
     }
     list.render(doc, type.value, "end");
 });
